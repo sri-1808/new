b0aac2a2b1f8a481613bd9cdaa1c8b82a54b10f4
Sowmya Sri <darapureddysowmyasri@gmail.com>
2023-07-28 11:58:45 +0530

    logging done

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-from setuptools import find_packages,setup
-from typing import List
+from setuptools import setup , find_packages 
+from typing import List 
 HYPEN_E_DOT='-e .'
-
 def get_requirements(file_path:str) -> List[str]:
     requirements=[]
-    with open (file_path) as file_obj:
+    with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
-
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     return requirements
+
 setup(
     name='new',
     version='0.0.1',
@@ -18,4 +17,4 @@ setup(
     author_email='darapureddysowmyasri@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
-    )
+)
